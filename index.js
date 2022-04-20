2733525d8ce807ac2fc2e4b2e46ea81e16ddae53
coderbennett <joeykb17@gmail.com>
2022-04-19 18:36:58 -0700

    replace questions array strings with objects

--- index.js
+++ index.js
@@ -4,19 +4,56 @@ const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is the title of your project?",
-    "Please give a description of your project.",
-    "How does someone install your project?",
-    "How is your project used?",
-    "Which license are you using?",
-    "What are your contribution guidelines?",
-    "What are the testing instructions?",
-    "What is your GitHub username?",
-    "What is your email?"
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please give a description of your project.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "How does someone install your project?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "How is your project used?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Which license are you using?",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What are your contribution guidelines?",
+        name: "contributions"
+    },
+    {
+        type: "input",
+        message: "What are the testing instructions?",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+}
 
 // TODO: Create a function to initialize app
 function init() {}
